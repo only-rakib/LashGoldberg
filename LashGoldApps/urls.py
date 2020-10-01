e709e7a68be04d42537e417d9562f197bcd05101
Rakibul Hasan <rakibulh170@gmail.com>
2020-10-01 21:59:11 +0600

    blog start

--- LashGoldApps/urls.py
+++ LashGoldApps/urls.py
@@ -4,5 +4,10 @@ urlpatterns = [
     path('', views.home_view, name='home'),
     path('law_firm/', views.law_firm_view, name='law_firm'),
     path('practices/', views.practice_view, name='practices'),
+    path('attorneys/', views.attorneys_view, name='attorneys'),
+    path('offices/', views.offices_view, name='offices'),
+    path('disclaimer/', views.disclaimer_view, name='disclaimer'),
+    path('sitemap/', views.sitemap_view, name='sitemap'),
+    path('blog/', views.blog_view, name='blog'),
 
 ]
--- LashGoldApps/views.py
+++ LashGoldApps/views.py
@@ -7,3 +7,13 @@ def law_firm_view(request):
     return render(request,'Law_firm.html')
 def practice_view(request):
     return render(request,'Practices.html')
+def attorneys_view(request):
+    return render(request,'Attorneys.html')
+def offices_view(request):
+    return render(request, 'Offices.html')
+def disclaimer_view(request):
+    return render(request, 'disclaimer.html')
+def sitemap_view(request):
+    return render(request, 'sitemap.html')
+def blog_view(request):
+    return render(request, 'blog.html')
